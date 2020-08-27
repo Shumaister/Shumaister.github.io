aa97fc343166f59b45bfc90eb9844e3041a066e1
shumaister <gabrielmaiori@gmail.com>
2020-08-27 02:15:36 -0300

    Updates

---
+++ precache-manifest.be20f56a6d8270e81a4332f627b1e4dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98284aba30cbd23efc748b8b584b9af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2a76058631f70f633f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "883ee4fa2401d20769ab",
+    "url": "/static/js/2.cf1a37e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf1a37e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2a76058631f70f633f",
+    "url": "/static/js/main.92abf430.chunk.js"
+  },
+  {
+    "revision": "178e626154d14e6531d7",
+    "url": "/static/js/runtime-main.2ecaf4ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
